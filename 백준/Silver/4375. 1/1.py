6c2d88381468be6dae13de73fa1e28d5dd01a5a7
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-02-11 23:37:43 +0900

    [Silver III] Title: 1, Time: 280 ms, Memory: 114976 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.py
+++ 백준/Silver/4375. 1/1.py
@@ -1,13 +1,13 @@
 while True:
-    try:
-        n = int(input()) # 3
-        digit = '1' # 자릿수
-        while True:
-            one = int(digit)
-            if one % n == 0:
-                print(len(str(one)))
-                break
-            else:
-                digit += '1' # 11, 111, 1111...
-    except:
+    try: 
+        n = int(input())
+    except: 
         break
+    s = 0 # sum
+    i = 0 # 자릿수
+    while True:
+        i += 1
+        s = s * 10 + 1
+        if s % n == 0:
+            print(i)
+            break
