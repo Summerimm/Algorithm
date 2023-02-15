2722d650e764e7b41dc5ec4d9d13990cc5a84655
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-02-15 14:30:24 +0900

    [D4] Title: 쇠막대기 자르기, Time: 306 ms, Memory: 90,440 KB -BaekjoonHub

---
+++ SWEA/D4/5432. 쇠막대기 자르기/README.md
@@ -0,0 +1,11 @@
+# [D4] 쇠막대기 자르기 - 5432 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWVl47b6DGMDFAXm) 
+
+### 성능 요약
+
+메모리: 90,440 KB, 시간: 306 ms, 코드길이: 396 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.py
@@ -0,0 +1,15 @@
+T = int(input())
+for tc in range(1, T+1):
+    p = list(input())
+    st = []
+    ans = 0
+    for i, c in enumerate(p):
+        if c == '(':
+            st.append(c)
+        elif c == ')' and st[-1] == p[i-1] == '(': # 레이저
+            st.pop()
+            ans += len(st)
+        elif c == ')' and p[i-1] == ')': # 쇠막대기의 끝
+            st.pop()
+            ans += 1
+    print(f'#{tc} {ans}')
