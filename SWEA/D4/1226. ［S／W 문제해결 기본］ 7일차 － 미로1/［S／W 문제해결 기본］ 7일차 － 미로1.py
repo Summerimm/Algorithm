37140f816950370c69994dbcdb74ab420eddf895
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-02-16 00:32:48 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 168 ms, Memory: 61,984 KB -BaekjoonHub

---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -0,0 +1,43 @@
+def dfs(si, sj):
+    st = []
+    di = [-1, 1, 0, 0]
+    dj = [0, 0, -1, 1]
+
+    ci, cj = si, sj     # 기준점
+    arr[ci][cj] = '1'   # 방문한 위치를 벽으로 처리
+
+    while True:
+        for k in range(4):
+            vi, vj = ci+di[k], cj+dj[k]
+            # 미로 범위 내/0 혹은 3(미방문)
+            if 0 <= vi < N and 0 <= vj < N and arr[vi][vj] != '1':
+                st.append((ci, cj))   # 부모노드(돌아올 지점) 저장
+                arr[vi][vj] = '1'   # 자식노드 방문처리
+                ci, cj = vi, vj     # 기준점 변경(자식노드 ni, nj)로
+                break               # 이동 경로 찾자마자 실행
+        else:
+            if st: # 돌아갈 지점 존재
+                ci, cj = st.pop()
+            else:  # 돌아갈 지점 X
+                break
+
+T = 10
+for tc in range(T):
+    N = 16
+    tc = int(input())
+    arr = [list(input()) for _ in range(N)]
+
+    # 시작 노드 찾기
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == '2':
+                si, sj = i, j
+            elif arr[i][j] == '3':
+                ei, ej = i, j
+
+    dfs(si, sj)
+    ans = arr[ei][ej]
+    # 도착지점에 3이 그대로 적혀있으면 방문 실패했다는 의미
+    if ans == '3':
+        ans = '0'
+    print(f'#{tc} {ans}')
