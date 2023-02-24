0862daf40cca3d080105a81926661d6595f81f8c
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-02-24 11:48:28 +0900

    [D4] Title: 정사각형 방, Time: 320 ms, Memory: 64,128 KB -BaekjoonHub

---
+++ SWEA/D4/1861. 정사각형 방/README.md
@@ -0,0 +1,11 @@
+# [D4] 정사각형 방 - 1861 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LtJYKDzsDFAXc) 
+
+### 성능 요약
+
+메모리: 64,128 KB, 시간: 320 ms, 코드길이: 975 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -0,0 +1,28 @@
+def bfs(si, sj):
+    q = [(si, sj)]
+    roomlst = [arr[si][sj]]
+    visited[si][sj] = 1
+    while q:
+        ti, tj = q.pop(0)
+        for di, dj in  [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+            ni, nj = ti + di, tj + dj
+            if 0<=ni<N and 0<=nj<N and abs(arr[ti][tj] - arr[ni][nj]) == 1 and visited[ni][nj] == 0:
+                q.append((ni, nj))
+                roomlst.append(arr[ni][nj])
+                visited[ni][nj] = 1         # 방문 처리
+    return min(roomlst), len(roomlst)       # 루트를 하나 찾고 그 루트에서 가장 작은 값, 길이 반환
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    visited = [[0] * N for _ in range(N)]
+    
+    cnt = 0
+    num = 1000000
+    for i in range(N):
+        for j in range(N):
+            tnum, tcnt = bfs(i, j)
+            if tcnt > cnt or (tcnt == cnt and tnum < num):
+                cnt, num = tcnt, tnum
+    print(f'#{tc} {num} {cnt}')
