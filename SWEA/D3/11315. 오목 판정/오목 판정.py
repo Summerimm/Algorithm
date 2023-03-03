308b1041180e81a528844044028561f6e3b6a2e6
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-03-03 17:49:52 +0900

    [D3] Title: 오목 판정, Time: 209 ms, Memory: 62,788 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,55 +1,23 @@
+def diagonal(arr):
+    for i in range(N-5+1):
+        for j in range(N-5+1):
+            tmp1 = ''.join((arr[i][j], arr[i+1][j+1], arr[i+2][j+2], arr[i+3][j+3], arr[i+4][j+4]))
+            if tmp1 == 'o' * 5:
+                return True
+    return False
+
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
-    omok = [input() for _ in range(N)]  # 원래 오목 행렬
-
-    omokt = []      # # 오목 전치행렬
-    tmp = list(zip(*omok))
-    for i in range(N):
-        omokt.append(''.join(tmp[i]))
-
-    omokd1 = []     # 오목 좌상우하 대각선 행렬
-    for r in range(N-1, -1, -1): # 기준점
-        st = ''
-        c = 0
-        while 0<=r<N and 0<=c<N:
-            st += omok[r][c]
-            r += 1
-            c += 1
-        omokd1.append(st)
-    for c in range(1, N):
-        st = ''
-        r = 0
-        while 0<=c<N and 0<=c<N:
-            st += omok[r][c]
-            r += 1
-            c += 1
-        omokd1.append(st)
-
-    omokd2 = []     # 오목 우상좌하 대각선 행렬
-    for r in range(N): # 기준점
-        st = ''
-        c = N-1
-        while 0<=r<N and 0<=c<N:
-            st += omok[r][c]
-            r += 1
-            c -= 1
-        omokd2.append(st)
-    for c in range(N-2, -1, -1):
-        st = ''
-        r = 0
-        while 0<=c<N and 0<=c<N:
-            st += omok[r][c]
-            r += 1
-            c -= 1
-        omokd2.append(st)
+    arr = [input() for _ in range(N)]
+    arrt = [''.join(c) for c in zip(*arr)]
 
     ans = 'NO'
-    for i in range(N):
-        if 'ooooo' in omok[i] or 'ooooo' in omokt[i]:
+    for st in arr + arrt:
+        if 'o'*5 in st:
             ans = 'YES'
-    for j in range(2*N-1):
-        if 'ooooo' in omokd1[j] or 'ooooo' in omokd2[j] :
+            break 
+    else:
+        if diagonal(arr) or diagonal(arrt[::-1]):
             ans = 'YES'
-
     print(f'#{tc} {ans}')
