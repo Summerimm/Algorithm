abafe067ddb2c37822e1031df18ccd0b4bf1d7f0
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-03-03 14:25:48 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println(str + "??!");
+    }
+}
