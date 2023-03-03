69d2e34f0f08c775deede5908e1b51fdb2ecad5b
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-03-03 14:50:21 +0900

    [Bronze III] Title: 곱셈, Time: 204 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException{
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = b;
 
-        for (int i=0;i<3;i++){
-            int r = b % 10;
-            b /= 10;
-            System.out.println(a * r);
-        }
-        System.out.println(a * c);
+        int a = sc.nextInt();
+        String b = sc.next();
+
+        sc.close();
+
+        System.out.println(a * (b.charAt(2) - '0'));
+        System.out.println(a * (b.charAt(1) - '0'));
+        System.out.println(a * (b.charAt(0) - '0'));
+        System.out.println(a * Integer.parseInt(b));
     }
 }
