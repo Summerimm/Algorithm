03848623e76f6d49b5ce7dc708cda9a7551cc856
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-03-03 17:11:15 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 224 ms, Memory: 68,064 KB -BaekjoonHub

---
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -0,0 +1,10 @@
+T = 10
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [list(input()) for _ in range(N)]
+    arrt = list(zip(*arr))
+
+    ans = 0
+    for c in arrt:
+        ans += ''.join(c).replace('0', '').count('12')
+    print(f'#{tc} {ans}')
