9f3138b091486acf56716295d39b827f2eeec7b9
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-03-07 09:01:05 +0900

    [Bronze II] Title: 줄 세우기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.py
@@ -0,0 +1,15 @@
+N = int(input())
+arr = list(map(int, input().split()))
+ans = [0] * (N-1) + [1]
+
+for i in range(1, N):
+    k = arr[i]
+    if k == 0:
+        ans.append(i+1)
+    else:
+        ans[-i-1:-k-1] = ans[-i:-k]
+        ans[-k-1] = i+1
+for a in ans:
+    if a != 0:
+        print(a, end=' ')
+print()
