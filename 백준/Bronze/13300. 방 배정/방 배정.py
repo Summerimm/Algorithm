42829e483c249fa0e3fb8f5788b8d19b43dc76bf
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-03-07 09:04:11 +0900

    [Bronze II] Title: 방 배정, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+arr = [0] * 14
+for i in range(N):
+    s, y = map(int, input().split())
+    arr[2*y+s] += 1
+
+ans = 0
+for cnt in arr:
+    if 0 < cnt <= K:
+        ans += 1
+    else:
+        ans += cnt // K
+        if cnt % K != 0:
+            ans += 1
+print(ans)
