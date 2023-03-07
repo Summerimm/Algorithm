cb2db1a4ab50c15ce0302df9cdc6c576c4bd755b
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-03-07 09:03:56 +0900

    [Bronze II] Title: 영식이와 친구들, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.py
@@ -0,0 +1,16 @@
+N, M, L = map(int, input().split())
+arr = [0] * (N+1)
+arr[1] = 1
+s = 1
+
+while M not in arr:
+    if arr[s] % 2:
+        s -= L % N
+        if s < 1:
+            s += N
+    else:
+        s += L % N
+        if s > N:
+            s -= N
+    arr[s] += 1
+print(sum(arr) - 1)
