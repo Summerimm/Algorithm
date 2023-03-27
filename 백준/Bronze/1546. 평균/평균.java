9480ff6f844a3b16a415029aa1f3c85e7c39f852
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-03-27 17:40:18 +0900

    [Bronze I] Title: 평균, Time: 136 ms, Memory: 14680 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int mx = 0;
+        double[] arr = new double[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++){
+            int value = Integer.parseInt(st.nextToken());
+            arr[i] = value;
+
+            if (value > mx){
+                mx = value;
+            }
+        }
+
+        for (int i=0; i<N; i++) {
+            arr[i] = arr[i] / mx * 100;
+        }
+
+        double avg = Arrays.stream(arr).average().orElse(0);
+        System.out.println(avg);
+    }
+}
