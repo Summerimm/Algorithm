b2a4b6a377e07efdcb6468334be058d1a7a82809
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-03-28 13:38:15 +0900

    [D3] Title: 화물 도크, Time: 168 ms, Memory: 58,504 KB -BaekjoonHub

---
+++ SWEA/D3/13071. 화물 도크/README.md
@@ -0,0 +1,11 @@
+# [D3] 화물 도크 - 13071 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXxOiEN6SU0DFASZ) 
+
+### 성능 요약
+
+메모리: 58,504 KB, 시간: 168 ms, 코드길이: 380 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/13071. 화물 도크/화물 도크.py
@@ -0,0 +1,17 @@
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [[0] * 2 for _ in range(N)]
+
+    for i in range(N):
+        arr[i][0], arr[i][1] = map(int, input().split())
+    arr = sorted(arr, key=lambda x:x[1])
+
+    cnt = 0
+    s, e = 0, 0
+    for t in arr:
+        if t[0] >= e:
+            cnt += 1
+            s = t[0]
+            e = t[1]
+    print(f'#{tc} {cnt}')
