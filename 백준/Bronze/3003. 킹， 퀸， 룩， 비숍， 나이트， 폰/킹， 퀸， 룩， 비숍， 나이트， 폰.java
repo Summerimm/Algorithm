16a8a6643654dd6f5300f49d41aa2f75d68b7d43
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-06-23 21:46:21 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 144 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int k = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+
+        System.out.print(1 - k + " ");
+        System.out.print(1 - q + " ");
+        System.out.print(2 - r + " ");
+        System.out.print(2 - b + " ");
+        System.out.print(2 - n + " ");
+        System.out.print(8 - p);
+        System.out.println();
+    }
+}
