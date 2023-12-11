aff757e1e5cc380e9dc8132c985c49417d8ad3cc
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-12-11 23:17:43 +0900

    [Bronze III] Title: 직각삼각형, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,10 @@
+while True:
+    lst = list(map(int, input().split()))
+    if lst[0] == lst[1] == lst[2] == 0:
+        break
+    else:
+        lst.sort()
+        if lst[0] ** 2 + lst[1] ** 2 == lst[2] ** 2:
+            print('right')
+        else:
+            print('wrong')
