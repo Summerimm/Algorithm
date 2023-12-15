5284a455ae7cde46fa9fe305b523ac50c1bf1e92
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-12-15 22:49:50 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 4260 ms, Memory: 128156 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,9 @@
+N = int(input())
+arr = [0] * 10001
+for _ in range(N):
+    arr[int(input())] += 1
+
+for i in range(10001):
+    if arr[i] != 0:
+        for j in range(arr[i]):
+            print(i)
