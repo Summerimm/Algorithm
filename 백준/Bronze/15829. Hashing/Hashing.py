3d652b954cf2e0bf484b0c74a6157d3440302055
Harim Kim <108309396+Haaarimmm@users.noreply.github.com>
2023-12-17 22:14:47 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,7 +1,7 @@
 L = int(input())
-arr = list(map(str, input()))
+string = input()
 
 ans = 0
 for i in range(L):
-    ans += (ord(arr[i]) - 96) * (31 ** i)
-print(ans)
+    ans += (ord(string[i]) - 96) * (31 ** i)
+print(ans % 1234567891)
