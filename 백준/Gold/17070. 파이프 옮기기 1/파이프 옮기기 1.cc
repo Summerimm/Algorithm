1bbb9e96718057f6efee6113fddd7bfea876edc9
Harim Kim <108309396+Summerimm@users.noreply.github.com>
2024-06-06 23:45:01 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+
+int N;
+int map[16][16];
+int dp[16][16][3];
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+        {
+            cin >> map[i][j];
+            dp[i][j][0] = dp[i][j][1] = dp[i][j][2] = 0;
+        }
+
+    dp[0][1][0] = 1;
+    for (int j = 2; j < N; j++)
+    {
+        if (!map[0][j])
+            dp[0][j][0] = dp[0][j - 1][0];
+    }
+
+    for (int i = 1; i < N; i++)
+        for (int j = 2; j < N; j++)
+        {
+            if (!map[i][j])
+            {
+                dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+                dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+                if (!map[i - 1][j] && !map[i][j - 1])
+                    dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+            }
+        }
+
+    cout << dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2] << endl;
+    return 0;
+}
